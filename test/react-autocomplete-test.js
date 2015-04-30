fb296eb510484f80168260feacef4fa1043ea944
Talysson <talyssonoc@gmail.com>
2015-04-30 11:11:51 -0300

    Update tests

--- test/react-autocomplete-test.js
+++ test/react-autocomplete-test.js
@@ -4,31 +4,32 @@ var loremPath = './support/lorem.json';
 jest.dontMock(autoCompletePath);
 jest.dontMock(loremPath);
 
-// var React = require('react/addons');
-// var TestUtils = React.addons.TestUtils;
-// var AutoComplete = require(autoCompletePath);
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+var AutoComplete = require(autoCompletePath);
 var lorem = require(loremPath);
 
 describe('AutoComplete', function() {
 
   it('shows options when entered "l" on input', function() {
-    expect(2).toEqual(50);
 
-    // var autoComplete = TestUtils.renderIntoDocument(
-    //   <AutoComplete words={lorem}/>
-    // );
+    var autoComplete = TestUtils.renderIntoDocument(
+      <AutoComplete words={lorem}/>
+    );
 
-    // var input = TestUtils.findRenderedDOMComponentWithTag(autoComplete, 'input');
+    var input = TestUtils.findRenderedDOMComponentWithTag(autoComplete, 'input');
 
-    // TestUtils.Simulate.change(input, {
-    //   keyCode: 108
-    // });
+    input.getDOMNode().value = 'l';
 
-    // var list = TestUtils.findRenderedDOMComponentWithClass(autoComplete, 'autocomplete-list');
+    TestUtils.Simulate.change(input, {
+      keyCode: 108
+    });
 
-    // var items = TestUtils.scryRenderedDOMComponentsWithClass(list, 'autocomplete-item');
+    var list = TestUtils.findRenderedDOMComponentWithClass(autoComplete, 'autocomplete-list');
 
-    // expect(items.length).toEqual('3');
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(autoComplete, 'autocomplete-item');
+
+    expect(items.length).toEqual(3);
   });
 
 });
