03b7a7d41aff130bc10687bdef2c7b6359a9b9c6
Talysson <talyssonoc@gmail.com>
2015-05-01 05:48:36 -0300

    Add new test

--- test/react-autocomplete-test.js
+++ test/react-autocomplete-test.js
@@ -21,15 +21,53 @@ describe('AutoComplete', function() {
 
     input.getDOMNode().value = 'l';
 
+    // Types "l"
     TestUtils.Simulate.change(input, {
       keyCode: 108
     });
 
     var list = TestUtils.findRenderedDOMComponentWithClass(autoComplete, 'autocomplete-list');
-
     var items = TestUtils.scryRenderedDOMComponentsWithClass(autoComplete, 'autocomplete-item');
 
-    expect(items.length).toEqual(3);
+    expect(items.length).toBe(3);
+
+    var expectedItems = [
+      'labore',
+      'laboris',
+      'laborum'
+    ];
+
+    expectedItems.forEach(function(item, index) {
+      expect(item).toEqual(items[index].getDOMNode().textContent);
+    });
+  });
+
+  it('adds hover class to element selected with arrows', function() {
+    var autoComplete = TestUtils.renderIntoDocument(
+      <AutoComplete words={lorem}/>
+    );
+
+    var input = TestUtils.findRenderedDOMComponentWithTag(autoComplete, 'input');
+
+    input.getDOMNode().value = 'l';
+
+    // Types "l"
+    TestUtils.Simulate.change(input, {
+      keyCode: 180
+    });
+
+    // Clicks arrow down
+    TestUtils.Simulate.keyDown(input, {
+      keyCode: 40
+    });
+
+    var list = TestUtils.findRenderedDOMComponentWithClass(autoComplete, 'autocomplete-list');
+    var items = TestUtils.scryRenderedDOMComponentsWithClass(autoComplete, 'autocomplete-item');
+
+    expect(items[0].getDOMNode().className).toContain('hover');
+    expect(items[1].getDOMNode().className).not.toContain('hover');
+    expect(items[2].getDOMNode().className).not.toContain('hover');
+
   });
 
 });
